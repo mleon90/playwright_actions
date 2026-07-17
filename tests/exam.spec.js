fbfb4286a29dee1d27a2620eb1788b5f5cddfeef
Mariana <mariana.leon1025@gmail.com>
2026-07-17 10:57:59 -0500

    iniciando proyecto git actions

---
+++ tests/exam.spec.js
@@ -0,0 +1,21 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+// Execute Playwright Test | Run | Debug | Inspect
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+// Execute Playwright Test | Run | Debug | Inspect
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+});
