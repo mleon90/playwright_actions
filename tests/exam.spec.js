cfdab17cc9a99a064608f6b6f53a87b7547b12a1
Mariana <mariana.leon1025@gmail.com>
2026-07-17 21:55:49 -0500

    ajuste examp

--- tests/exam.spec.js
+++ tests/exam.spec.js
@@ -19,3 +19,13 @@ test('get started link', async ({ page }) => {
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
 });
+
+test('test tres de pruebas', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+});
